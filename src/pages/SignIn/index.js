cecdf80fc8824be64139a33c79a6de53d4e6ceae
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-01-28 13:45:36 -0300

    Implementing UnForm

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/images/agerba.svg';
 
 export default function SignIn() {
+  function handlSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Portal Estudantil" width="200" />
 
-      <form>
-        <input
+      <Form onSubmit={handlSubmit}>
+        <Input
+          name="email"
           type="email"
           placeholder="Seu e-mail"
           autofocus="true"
+          autoComplete="off"
           required
         />
-        <input type="password" name="" id="" placeholder="Seu password" />
+        <Input
+          name="password"
+          type="password"
+          name=""
+          id=""
+          placeholder="Seu password"
+        />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/images/agerba.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Portal Estudantil" width="200" />
 
-      <form>
-        <input placeholder="Nome completo" autoComplete="off" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" name="" id="" placeholder="Seu password" />
+      <Form onSubmit={handleSubmit}>
+        <Input
+          name="name"
+          placeholder="Nome completo"
+          autoComplete="off"
+          autoFocus
+        />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          name=""
+          id=""
+          placeholder="Seu password"
+        />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já possuo login</Link>
-      </form>
+      </Form>
     </>
   );
 }
