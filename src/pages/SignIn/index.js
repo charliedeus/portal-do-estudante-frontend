0ed340580726a927513f39b4927b75eb9ca8a6c1
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-01-28 09:09:36 -0300

    Styling Auth Page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
+import logo from '~/assets/images/agerba.svg';
 
 export default function SignIn() {
   return (
     <>
-      <h1>SignIn Page</h1>
+      <img src={logo} alt="AGERBA" width="210" />
+
+      <form action="">
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Seu password" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta</Link>
+      </form>
     </>
   );
 }
