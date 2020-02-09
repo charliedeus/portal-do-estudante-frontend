f5d294ebb5bdd4a60ed745e9bab3bab48af93761
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-02-09 17:43:10 -0300

    Profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
     <>
-      <h1>Profile Page</h1>
+      <Container>
+        <Form autoComplete="off" initialData={profile} onSubmit={handleSubmit}>
+          <Input name="name" placeholder="Nome completo" autoFocus />
+          <Input name="email" type="email" placeholder="Seu e-mail" />
+
+          <hr />
+
+          <Input
+            name="oldPassword"
+            type="password"
+            placeholder="Seu password atual"
+          />
+          <Input name="password" type="password" placeholder="Novo password" />
+          <Input
+            name="confirmPassword"
+            type="password"
+            placeholder="Confirme novo password"
+          />
+
+          <button type="submit">Atualizar perfil</button>
+        </Form>
+        <button type="submit">Sair do sistema</button>
+      </Container>
     </>
   );
 }
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(137, 196, 244, 1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #333;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: #333;
+      }
+    }
+
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 10px 0 20px;
+    }
+
+    span {
+      color: white;
+      font-weight: bold;
+      margin: 0 0 10px;
+      align-self: flex-start;
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #369eff;
+      font-weight: bold;
+      color: #333;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.1, '#369eff')};
+      }
+    }
+
+    a {
+      color: #333;
+      margin-top: 15px;
+      font-size: 16px;
+      font-weight: bold;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+
+  > button {
+    width: 100%;
+    margin: 10px 0 0;
+    height: 44px;
+    background: #f64c75;
+    font-weight: bold;
+    color: #333;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.1, '#F64C75')};
+    }
+  }
+`;
